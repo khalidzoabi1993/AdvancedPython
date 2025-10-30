059071947f927877399311a2bad0b217a3b180cb
Khalid Zoabi <khalidzoabi1993@gmail.com>
2025-10-30 23:36:01 +0200

    first commit

---
+++ tergol1/strings.py
@@ -0,0 +1,12 @@
+sentence = "When you have a dream, you've got to grab it and never let go."
+print(sentence[-3:])
+# print(sentence)
+print(sentence[0])
+#
+#
+print(sentence[2:5])
+# explain sentence[2:5]: It is the third character to the fifth character in the string
+print(sentence[1], sentence[-2])
+print(sentence[1])
+print(sentence[-2])
+# explain sentence[-2]: It is the second last character in the string
